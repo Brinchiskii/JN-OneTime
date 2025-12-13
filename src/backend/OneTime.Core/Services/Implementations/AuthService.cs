52f0576d291fa8cdab04505b4955989ab4711425
abdiabbaz <abs001@edu.zealand.dk>
2025-12-13 14:15:55 +0100

    done login

--- src/backend/OneTime.Core/Services/Implementations/AuthService.cs
+++ src/backend/OneTime.Core/Services/Implementations/AuthService.cs
@@ -13,20 +13,20 @@ namespace OneTime.Core.Services.Implementations
 {
 	public class AuthService : IAuthService
 	{
-		private readonly IUserRepository _users;
-		private readonly IPasswordHasher _hasher;
+		private readonly IUserRepository _usersRepository;
+		private readonly IPasswordHasher _hasherRepository;
 		private readonly IConfiguration _config;
 
 		public AuthService(IUserRepository users, IPasswordHasher hasher, IConfiguration config)
 		{
-			_users = users;
-			_hasher = hasher;
+			_usersRepository = users;
+			_hasherRepository = hasher;
 			_config = config;
 		}
 
 		public async Task<JNUser> Register(string name, string email, string password ,UserRole role, int? managerId)
 		{
-			var existing = await _users.GetByEmail(email);
+			var existing = await _usersRepository.GetByEmail(email);
 			if (existing != null)
 				throw new InvalidOperationException("Email is already in use.");
 
@@ -35,18 +35,18 @@ namespace OneTime.Core.Services.Implementations
 			if (role != UserRole.Employee && managerId.HasValue)
 				throw new InvalidOperationException("Only employees can have a manager.");
 
-			var (hash, generatedSalt) = _hasher.HashPassword(password);
+			var (hash, generatedSalt) = _hasherRepository.HashPassword(password);
 
-			return await _users.Create(name, email, hash, generatedSalt, role, managerId);
+			return await _usersRepository.Create(name, email, hash, generatedSalt, role, managerId);
 		}
 
 		public async Task<(JNUser User, string Token)> Login(string email, string password)
 		{
-			var user = await _users.GetByEmail(email);
+			var user = await _usersRepository.GetByEmail(email);
 			if (user == null)
 				throw new InvalidOperationException("Invalid credentials.");
 
-			var ok = _hasher.VerifyPassword(password, user.PasswordHash, user.PasswordSalt);
+			var ok = _hasherRepository.VerifyPassword(password, user.PasswordHash, user.PasswordSalt);
 			if (!ok)
 				throw new InvalidOperationException("Invalid credentials.");
 
@@ -65,7 +65,7 @@ namespace OneTime.Core.Services.Implementations
 			new Claim(ClaimTypes.Name, user.Name),
 			new Claim(ClaimTypes.Email, user.Email),
 			new Claim(ClaimTypes.Role, ((UserRole)user.Role).ToString())
-        };
+			};
 
 			var token = new JwtSecurityToken(
 				issuer: _config["Jwt:Issuer"],
